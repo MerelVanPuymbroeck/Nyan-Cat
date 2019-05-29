ed17e83dc72fb1e47c7c56899c357b15284f0de9
Merel van puymbroeck <merelvanpuymbroeck@gmail.com>
2019-05-29 09:17:09 +0200

    Add files via upload

--- myscripts.js
+++ myscripts.js
@@ -5,6 +5,51 @@ btnBack.addEventListener('click',function() {
 });
 }
 
-var audio = new Audio();
-audio.src = "file_name.mp3";
-audio.play();
+function settime(){
+	var audio= document.getElementById("myaudio");
+	audio.currentTime=38;
+	audio.play();
+	console.log(audio.currentTime);
+	setInterval(function(){
+		if(audio.currentTime>45){
+			audio.pause();
+				}
+			},1000);
+		}
+
+  var canvas = document.getElementById("canvas");
+  canvas.width = window.innerWidth;
+  canvas.height = window. innerHeight;
+  var context = canvas.getContext("2d");
+  var img = new Image ();
+  img.src = "NyanCat.png";
+  var NoOfCats = 20;
+  var Cats = [];
+  for (var i=0; i<NoOfCats; i++) {
+    var x = Math.floor (Math.random()* canvas.width);
+    var y = Math.floor (Math.random()* canvas.height);
+    heart [i] = new Cats (x,y);
+  }
+
+
+    function Cats (x,y){
+      this.x = x;
+      this.y = y;
+
+      this.show = function() {
+      context.drawImage(img,this.x, this.y, 10,10);
+      }
+    }
+
+
+    function draw() {
+    context.fillStyle = "";
+    context.fillRect (0,0,canvas.width, canvas.height);
+    }
+
+    function update () {
+      draw();
+      window.requestAnimationFrame(update);
+    }
+
+    update ();
