2797b0d35e26674e977e237b1823a6261c8849d1
Merel van puymbroeck <merelvanpuymbroeck@gmail.com>
2019-06-03 09:22:51 +0200

    Add files via upload

--- myscripts.js
+++ myscripts.js
@@ -17,39 +17,43 @@ function settime(){
 			},1000);
 		}
 
-  var canvas = document.getElementById("canvas");
-  canvas.width = window.innerWidth;
-  canvas.height = window. innerHeight;
-  var context = canvas.getContext("2d");
-  var img = new Image ();
-  img.src = "NyanCat.png";
-  var NoOfCats = 20;
-  var Cats = [];
-  for (var i=0; i<NoOfCats; i++) {
-    var x = Math.floor (Math.random()* canvas.width);
-    var y = Math.floor (Math.random()* canvas.height);
-    heart [i] = new Cats (x,y);
+    var canvas = document.getElementById("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window. innerHeight;
+    var context = canvas.getContext("2d");
+    var img = new Image ();
+    img.src = "NyanCat.png";
+    var NoOfCats = 20;
+    var Cats = [];
+    for (var i=0; i<NoOfCats; i++) {
+      var x = Math.floor (Math.random()* canvas.width);
+      var y = Math.floor (Math.random()* canvas.height);
+      heart [i] = new Cats (x,y);
+    }
+
+
+      function Cats (x,y){
+        this.x = x;
+        this.y = y;
+
+        this.show = function() {
+        context.drawImage(img,this.x, this.y, 10,10);
+        }
+      }
+
+
+      function draw() {
+      context.fillStyle = "";
+      context.fillRect (0,0,canvas.width, canvas.height);
+      for (var i=0; i<NoOfCats;i++)
+    }
+      Cat[i].show ();
+    }
   }
 
-
-    function Cats (x,y){
-      this.x = x;
-      this.y = y;
-
-      this.show = function() {
-      context.drawImage(img,this.x, this.y, 10,10);
+      function update () {
+        draw();
+        window.requestAnimationFrame(update);
       }
-    }
 
-
-    function draw() {
-    context.fillStyle = "";
-    context.fillRect (0,0,canvas.width, canvas.height);
-    }
-
-    function update () {
-      draw();
-      window.requestAnimationFrame(update);
-    }
-
-    update ();
+      update ();
